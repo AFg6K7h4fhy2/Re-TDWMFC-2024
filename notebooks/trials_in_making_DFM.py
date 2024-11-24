5ab73896f4bf132b077abb4af75860a0a8e79883
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-11-24 14:26:38 -0500

    start adding in some math

--- notebooks/trials_in_making_DFM.py
+++ notebooks/trials_in_making_DFM.py
@@ -7,5 +7,34 @@ Model in Python.
 
 # %% IMPORTS
 
+import jax.numpy as jnp
 
-# %%
+# %% PARAMETERS FOR TURCHIN MODEL
+
+init_N = 0.5  # population
+init_S = 0.0  # accumulated state resources
+init_k = 1  # carry capacity of stateless population
+r = 0.02
+init_p = 1
+c = 3
+init_s = 10
+beta = 0.25
+
+# %% TURCHIN DFM
+
+
+def k(init_k, c, init_s, S):
+    """
+    State carrying capacity.
+    """
+    return init_k + (c * (S / (init_s + S)))
+
+
+def DFM(t, y, args):
+    N, S = y
+    dN = (r * N) * (1 - (N / k(S)))
+    dS = init_p * N * (1 - (N / k(S)))
+    return jnp.array([dN, dS])
+
+
+# %% PARAMETERS
