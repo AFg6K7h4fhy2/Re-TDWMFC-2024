463161df52840dda01e270e2e622f5b9b79605e8
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-12-02 21:47:59 -0500

    further code mods

--- notebooks/trials_in_making_DFM.py
+++ notebooks/trials_in_making_DFM.py
@@ -12,6 +12,7 @@ cliodynamics).
 
 import diffrax
 import jax.numpy as jnp
+import matplotlib.pyplot as plt
 
 # %% INITIAL VALUES FOR DFM
 
@@ -26,7 +27,7 @@ init_p = 1  # initial per capita taxation rate
 max_k = 4  # maximum carrying capacity
 c = max_k - init_k  # maximum gain of increasing carrying capacity
 init_s = 10  # initial state resources (grain)
-beta = 0.25  # per capita state expenditure
+beta = 0.1  # per capita state expenditure
 
 # %% STATE CARRYING CAPACITY
 
@@ -43,9 +44,10 @@ def k(S):
 
 def DFM(t, y, args):
     N, S = y  # get population and state resources
+    S = jnp.maximum(S, 0.0)  # ensure S >= 0
     r, init_p, beta = args  # get parameters
     dN = r * N * (1 - (N / k(S)))
-    dS = init_p * N * (1 - (N / k(S))) - (beta * N)
+    dS = (init_p * N * (1 - (N / k(S)))) - (beta * N)
     return jnp.array([dN, dS])
 
 
@@ -54,11 +56,23 @@ def DFM(t, y, args):
 term = diffrax.ODETerm(DFM)
 solver = diffrax.Tsit5()
 t0 = 0
-t1 = 100
+t1 = 500
 dt0 = 1
-y0 = (init_N, init_S)
-args = (r, init_p, beta)
-saveat = diffrax.SaveAt(ts=jnp.linspace(t0, t1, 100))
+y0 = jnp.array([init_N, init_S])
+args = jnp.array([r, init_p, beta])
+saveat = diffrax.SaveAt(ts=jnp.linspace(t0, t1, t1 - t0))
 sol = diffrax.diffeqsolve(
     term, solver, t0, t1, dt0, y0, args=args, saveat=saveat
 )
+N, S = sol.ys.T
+
+# %% PLOTTING
+
+figure, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+axes[0].plot(sol.ts, N, label="Population", color="orange")
+axes[0].legend()
+axes[1].plot(sol.ts, S, label="State Resources", color="green")
+axes[1].legend()
+plt.show()
+
+# %%
