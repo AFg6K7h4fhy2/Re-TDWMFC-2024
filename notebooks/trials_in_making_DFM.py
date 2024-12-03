833323a6c226d84737fbd82dca8ef08c387abebe
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-12-02 21:19:55 -0500

    add pyproject deps; add some DFM code

--- notebooks/trials_in_making_DFM.py
+++ notebooks/trials_in_making_DFM.py
@@ -1,40 +1,64 @@
 """
 This file contains code that the author used
 to understand and begin working on the
-implementation of the Demographic Wealth
-Model in Python.
+implementation of the Demographic Fiscal
+Model (DFM) in Python, with initial values
+and initial parameters values from the
+paper (The Demographic-Wealth model for
+cliodynamics).
 """
 
 # %% IMPORTS
 
+import diffrax
 import jax.numpy as jnp
 
-# %% PARAMETERS FOR TURCHIN MODEL
+# %% INITIAL VALUES FOR DFM
 
-init_N = 0.5  # population
+init_N = 0.5  # initial population
 init_S = 0.0  # accumulated state resources
+
+# %% PARAMETERS FOR DFM
+
 init_k = 1  # carry capacity of stateless population
-r = 0.02
-init_p = 1
-c = 3
-init_s = 10
-beta = 0.25
+r = 0.02  # rate of population growth
+init_p = 1  # initial per capita taxation rate
+max_k = 4  # maximum carrying capacity
+c = max_k - init_k  # maximum gain of increasing carrying capacity
+init_s = 10  # initial state resources (grain)
+beta = 0.25  # per capita state expenditure
 
-# %% TURCHIN DFM
+# %% STATE CARRYING CAPACITY
 
 
-def k(init_k, c, init_s, S):
+def k(S):
     """
     State carrying capacity.
     """
     return init_k + (c * (S / (init_s + S)))
 
 
+# %% DEMOGRAPHIC FISCAL MODEL
+
+
 def DFM(t, y, args):
-    N, S = y
-    dN = (r * N) * (1 - (N / k(S)))
-    dS = init_p * N * (1 - (N / k(S)))
+    N, S = y  # get population and state resources
+    r, init_p, beta = args  # get parameters
+    dN = r * N * (1 - (N / k(S)))
+    dS = init_p * N * (1 - (N / k(S))) - (beta * N)
     return jnp.array([dN, dS])
 
 
-# %% PARAMETERS
+# %% GET THE DFM SOLUTION
+
+term = diffrax.ODETerm(DFM)
+solver = diffrax.Tsit5()
+t0 = 0
+t1 = 100
+dt0 = 1
+y0 = (init_N, init_S)
+args = (r, init_p, beta)
+saveat = diffrax.SaveAt(ts=jnp.linspace(t0, t1, 100))
+sol = diffrax.diffeqsolve(
+    term, solver, t0, t1, dt0, y0, args=args, saveat=saveat
+)
