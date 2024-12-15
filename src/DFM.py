fix the derivative below 0 for S

src/DFM.py
@@ -78,9 +78,13 @@ def DFM(
     # capacity from state resources,
     # initial state resources
     r, init_rho, beta, init_k, c, init_s = args
-    dN = r * N * (1 - (N / k(S, init_k, c, init_s)))
+    dN = jnp.where(
+        S >= 0.0,
+        r * N * (1 - (N / k(S, init_k, c, init_s))),
+        r * N * (1 - (N / k(0.0, init_k, c, init_s))),
+    )
     dS = jnp.where(
-        S > 0.0,
+        S >= 0.0,
         (init_rho * N * (1 - (N / k(S, init_k, c, init_s)))) - (beta * N),
         0.0,
     )

src/DWM.py
@@ -86,6 +86,11 @@ def DWM(
     #  increase capacity from state wealth,
     # initial carrying capacity,
     r, beta, alpha, d, g, c, init_k = args
-    dN = (r * N * (1 - (N / k(S, init_k, c)))) - (alpha * S * (N / (d + N)))
-    dS = jnp.where(S > 0.0, (g * S * N) - (beta * S), 0.0)
+    dN = jnp.where(
+        S >= 0.0,
+        (r * N * (1 - (N / k(S, init_k, c)))) - (alpha * S * (N / (d + N))),
+        (r * N * (1 - (N / k(0.0, init_k, c))))
+        - (alpha * 0.0 * (N / (d + N))),
+    )
+    dS = jnp.where(S >= 0.0, (g * S * N) - (beta * S), 0.0)
     return jnp.array([dN, dS])